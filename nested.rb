2d57f4f16ee89849f757370678fff35b0b43a170
Abel Kalum <chumbabel@gmail.com>
2020-01-28 03:27:08 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -29,7 +29,7 @@ def alan_kay_is_known_for
           :languages => ["COBOL", "FORTRAN"]
         },
         :alan_kay => {
-          :known_for => "Object Orientation",
+           programmer_hash[:grace_hopper] => "Object Orientation",
           :languages => ["Smalltalk", "LISP"]
         },
         :dennis_ritchie => {
